f5f1f6e4f20dfc9254c58726f6bd5ec551b3f0e6
lancew <lw@judocoach.com>
2024-10-17 22:23:26 +0100

    Script was getting messey, moved code to module

--- MilleBornes.pl
+++ MilleBornes.pl
@@ -5,67 +5,24 @@ package MilleBornes;
 use Moo;
 use Game::Deckar;
 
+use lib 'lib';
+use MilleBornes::Game;
+
 use Data::Dumper;
+$Data::Dumper::Indent = $Data::Dumper::Sortkeys = 1;
 
-my $target_distance = 1000;
+my $mb_game = MilleBornes::Game->new();
 
-my @distance_cards = ( '25 KM', '50 KM', '75 KM', '100 KM', '200 KM', );
+my $target_distance = $mb_game->target_distance;
 
-my @hazard_cards
-    = ( 'Accident', 'Out of Gas', 'Flat Tire', 'Speed Limit', 'Stop', );
+my @distance_cards = @{ $mb_game->distances };
+my @hazard_cards   = @{ $mb_game->hazards };
+my @remedy_cards   = @{ $mb_game->remedies };
+my @safety_cards   = @{ $mb_game->safeties };
 
-my @remedy_cards
-    = ( 'Repairs', 'Gasoline', 'Spare Tire', 'End of Limit', 'Roll' );
+my $game = $mb_game->deck;
 
-my @safety_cards
-    = ( 'Driving Ace', 'Extra Tank', 'Puncture-Proof', 'Right of Way', );
-
-# cards start in the discard pile so that "draw" is empty which triggers
-# a card collection and shuffle
-
-my $game = Game::Deckar->new(
-    decks   => [ 'pile', 'discard' ],
-    initial => {
-        pile => [
-            ( $distance_cards[0] ) x 10,
-            ( $distance_cards[1] ) x 10,
-            ( $distance_cards[2] ) x 10,
-            ( $distance_cards[3] ) x 12,
-            ( $distance_cards[4] ) x 4,
-            ( $hazard_cards[0] ) x 3,
-            ( $hazard_cards[1] ) x 3,
-            ( $hazard_cards[2] ) x 3,
-            ( $hazard_cards[3] ) x 4,
-            ( $hazard_cards[4] ) x 5,
-            ( $remedy_cards[0] ) x 6,
-            ( $remedy_cards[1] ) x 6,
-            ( $remedy_cards[2] ) x 6,
-            ( $remedy_cards[3] ) x 6,
-            ( $remedy_cards[4] ) x 14,
-            ( $safety_cards[0] ) x 1,
-            ( $safety_cards[1] ) x 1,
-            ( $safety_cards[2] ) x 1,
-            ( $safety_cards[3] ) x 1,
-        ]
-    }
-);
-
-$game->shuffle('pile');
-
-my %players = (
-    'Player 1' => {
-        distance => 0,
-        hazards  => [],
-        remedies => [],
-        safety   => [],
-    },
-    'Player 2' => {
-        distance => 0,
-        hazards  => [],
-        remedies => [],
-        safety   => [],
-    },
-);
+my %players = %{ $mb_game->players };
 
 for my $player ( keys %players ) {
     $game->add_deck($player);
@@ -83,24 +40,27 @@ print "Race to $target_distance km\n";
 
 while ( !$game_over ) {
     for my $player ( sort keys %players ) {
-        # Player's turn
+
+        my $opponent = ( $player eq 'Player 1' ) ? 'Player 2' : 'Player 1';
         print "\n$player\'s turn:\n";
 
-        # Draw a card
-        #my $drawn_card = $game->pick('pile', $player, [0]);
         my ( $cards, $undo ) = $game->pick( 'pile' => $player, [0] );
         for my $c (@$cards) {
             print "$player drew: '$c'\n";
         }
 
     DISPLAY:
-        # Display player's hand
         print "Distance: $players{$player}{distance}\n";
 
+        print Dumper( \%players );
+
+        print "Can move: "
+            . ( $players{$player}{can_move} ? "Yes" : "No" ) . "\n";
+
         my @hand = @{ $game->get($player) };
+        push @hand, 'Discard';
         print "$player\'s hand: " . join( ", ", @hand ) . "\n";
 
-        # Ask player to choose a card to play
         print "Choose a card to play (enter the number): \n";
         for my $i ( 0 .. $#hand ) {
             print $i + 1 . ". $hand[$i]\n";
@@ -117,11 +77,27 @@ while ( !$game_over ) {
 
         my $played_card = $hand[ $choice - 1 ];
 
-        # Play the chosen card
+        if ( !$players{$player}{can_move} ) {
+            if ( !grep { $_ eq $played_card }
+                ( @safety_cards, @hazard_cards, 'Roll', 'Discard' ) )
+            {
+                print
+                    "\nYou cannot play this card when you cannot move. You need to play a 'Roll' card or a Safety card.\n";
+                print "Please choose another card.\n";
+                goto DISPLAY;
+            }
+        }
+
+        if ( $played_card eq 'Roll' ) {
+            $players{$player}{can_move} = 1;
+            print "$player rolled the dice and can move again.\n";
+            $game->pick( $player => 'discard', [ $choice - 1 ] );
+        }
+
         if ( $played_card =~ / KM$/ ) {
+
             my ($distance) = $played_card =~ /(\d+)/;
 
-# player can only play a distance card if their new total would be <= the target_distance
             if ( $players{$player}{distance} + $distance > $target_distance )
             {
                 print
@@ -133,25 +109,68 @@ while ( !$game_over ) {
             $players{$player}{distance} += $distance;
             print
                 "$player played $played_card. Total distance: $players{$player}{distance}\n";
-        }
-        elsif ( $played_card =~ /^Hazard/ ) {
-            # Implement hazard card logic
-            print "$player played $played_card on opponent.\n";
-        }
-        elsif ( $played_card =~ /^Remedy/ ) {
-            # Implement remedy card logic
-            print "$player played $played_card.\n";
-        }
-        elsif ( $played_card =~ /^Safety/ ) {
-            # Implement safety card logic
-            print "$player played $played_card.\n";
+
+            $game->pick( $player => 'discard', [ $choice - 1 ] );
         }
 
-        # Remove the played card from hand
-        $game->pick( $player => 'discard', [ $choice - 1 ] );
+        if ( $played_card eq 'Stop' || $played_card eq 'Speed Limit' ) {
+            unless ( grep { $_ eq 'Right of Way' }
+                @{ $players{$opponent}{safety} } )
+            {
+                push @{ $players{$opponent}{hazards} }, $played_card;
+                $players{$opponent}{can_move} = 0
+                    if $played_card eq 'Stop';
+                $game->pick( $player => 'discard', [ $choice - 1 ] );
+            }
+            else {
+                print
+                    "$opponent is protected by Right of Way. Hazard not applied.\n";
+            }
+        }
+        elsif ( $played_card eq 'Out of Gas' ) {
+            unless ( grep { $_ eq 'Extra Tank' }
+                @{ $players{$opponent}{safety} } )
+            {
+                push @{ $players{$opponent}{hazards} }, $played_card;
+                $players{$opponent}{can_move} = 0;
+                print "$opponent is out of gas and cannot move!\n";
+                $game->pick( $player => 'discard', [ $choice - 1 ] );
+            }
+            else {
+                print
+                    "$opponent is protected by Extra Tank. Hazard not applied.\n";
+            }
+        }
+        elsif ( $played_card eq 'Flat Tire' ) {
+            unless ( grep { $_ eq 'Puncture-proof' }
+                @{ $players{$opponent}{safety} } )
+            {
+                push @{ $players{$opponent}{hazards} }, $played_card;
+                $players{$opponent}{can_move} = 0;
+                print "$opponent has a flat tire and cannot move!\n";
+                $game->pick( $player => 'discard', [ $choice - 1 ] );
+            }
+            else {
+                print
+                    "$opponent is protected by Puncture-proof. Hazard not applied.\n";
+            }
+        }
+        elsif ( $played_card eq 'Accident' ) {
+            unless ( grep { $_ eq 'Driving Ace' }
+                @{ $players{$opponent}{safety} } )
+            {
+                push @{ $players{$opponent}{hazards} }, $played_card;
+                $players{$opponent}{can_move} = 0;
+                print "$opponent has been in an Accident and cannot move!\n";
+                $game->pick( $player => 'discard', [ $choice - 1 ] );
+            }
+            else {
+                print
+                    "$opponent is protected by Driving Ace. Hazard not applied.\n";
+            }
+        }
 
-        # Discard a card if hand size is still over 6
-        if ( scalar( $game->get($player) ) > 6 ) {
+        if ( scalar( @{ $game->get($player) } ) > 6 ) {
             print "Choose a card to discard (enter the number): \n";
             @hand = @{ $game->get($player) };
 
@@ -171,22 +190,20 @@ while ( !$game_over ) {
             $game->pick( $player => 'discard', [ $choice - 1 ] );
             print "$player discarded $discarded_card\n";
         }
-        # Player's turn logic goes here
-        # This is where you'd implement the game mechanics for each turn
 
-        # Check if the player has reached the target distance
         if ( $players{$player}{distance} >= $target_distance ) {
             $game_over = 1;
-            last;    # Exit the player loop
+            last;
         }
 
         $game->pick( 'pile' => $player, [0] );
     }
 }
 
-# Game has ended, determine the winner
-my $winner = ( sort { $players{$b}{distance} <=> $players{$a}{distance} }
-        keys %players )[0];
+my $winner = (
+    sort { $players{$b}{distance} <=> $players{$a}{distance} }
+        keys %players
+)[0];
 print "$winner has won the game!\n";
 
 1;
---
+++ lib/MilleBornes/Game.pm
@@ -0,0 +1,112 @@
+package MilleBornes::Game;
+
+use Moo;
+use Game::Deckar;
+
+has 'target_distance' => (
+    is      => 'ro',
+    default => 1000,
+);
+
+has 'hazards' => (
+    is      => 'ro',
+    default => sub {
+        return [ 'Accident', 'Out of Gas', 'Flat Tire', 'Speed Limit',
+            'Stop' ];
+    },
+);
+
+has 'distances' => (
+    is      => 'ro',
+    default => sub {
+        return [ '25 KM', '50 KM', '75 KM', '100 KM', '200 KM' ];
+    },
+);
+
+has 'remedies' => (
+    is      => 'ro',
+    default => sub {
+        return [ 'Repairs', 'Gasoline', 'Spare Tire', 'End of Limit',
+            'Roll' ];
+    },
+);
+
+has 'safeties' => (
+    is      => 'ro',
+    default => sub {
+        return [
+            'Driving Ace',
+            'Extra Tank',
+            'Puncture-Proof',
+            'Right of Way'
+        ];
+    },
+);
+
+has 'players' => (
+    is      => 'ro',
+    default => sub {
+        return {
+            'Player 1' => {
+                distance => 0,
+                hazards  => [],
+                remedies => [],
+                safety   => [],
+                can_move => 0,
+            },
+            'Player 2' => {
+                distance => 0,
+                hazards  => [],
+                remedies => [],
+                safety   => [],
+                can_move => 0,
+            },
+        };
+    },
+);
+
+has 'deck' => (
+    is      => 'lazy',
+    builder => '_build_deck',
+
+);
+
+sub _build_deck {
+    my $self = shift;
+
+    my $deck = Game::Deckar->new(
+        decks   => [ 'pile', 'discard' ],
+        initial => {
+            pile => [
+                ( $self->distances->[0] ) x 10,
+                ( $self->distances->[1] ) x 10,
+                ( $self->distances->[2] ) x 10,
+                ( $self->distances->[3] ) x 12,
+                ( $self->distances->[4] ) x 4,
+
+                ( $self->hazards->[0] ) x 3,
+                ( $self->hazards->[1] ) x 3,
+                ( $self->hazards->[2] ) x 3,
+                ( $self->hazards->[3] ) x 4,
+                ( $self->hazards->[4] ) x 5,
+
+                ( $self->remedies->[0] ) x 6,
+                ( $self->remedies->[1] ) x 6,
+                ( $self->remedies->[2] ) x 6,
+                ( $self->remedies->[3] ) x 6,
+                ( $self->remedies->[4] ) x 14,
+
+                ( $self->safeties->[0] ) x 1,
+                ( $self->safeties->[1] ) x 1,
+                ( $self->safeties->[2] ) x 1,
+                ( $self->safeties->[3] ) x 1,
+            ]
+        }
+    );
+
+    $deck->shuffle('pile');
+
+    return $deck;
+}
+
+1;
